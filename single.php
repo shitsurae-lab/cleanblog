6b8a07c7305985d25f3b38958b1c0e4edf034056
shitsurae-lab <20566363+shitsurae-lab@users.noreply.github.com>
2021-11-07 20:52:33 +0900

    single.phpバグ回避のif & endif

--- single.php
+++ single.php
@@ -7,37 +7,41 @@
 
 <body>
   <?php get_template_part('includes/header'); ?>
-  <!-- Page Header -->
-  <!-- ループの開始(投稿を出力する時に必ず必要) -->
-  <?php while (have_posts()) : the_post(); ?>
-    <header class="masthead" style="background-image: url('img/post-bg.jpg')">
-      <div class="overlay"></div>
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-8 col-md-10 mx-auto">
-            <div class="post-heading">
-              <h1><?php the_title(); ?></h1>
-              <span class="meta">Posted by
-                <?php the_author(); ?>
-                on <?php the_time(get_option('date-format')); ?></span>
+  <!-- 念の為 バグ回避 if -->
+  <?php if (have_posts()) : ?>
+    <!-- Page Header -->
+    <!-- ループの開始(投稿を出力する時に必ず必要) -->
+    <?php while (have_posts()) : the_post(); ?>
+      <header class="masthead" style="background-image: url('img/post-bg.jpg')">
+        <div class="overlay"></div>
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-8 col-md-10 mx-auto">
+              <div class="post-heading">
+                <h1><?php the_title(); ?></h1>
+                <span class="meta">Posted by
+                  <?php the_author(); ?>
+                  on <?php the_time(get_option('date-format')); ?></span>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </header>
+      </header>
 
-    <!-- Post Content -->
-    <article>
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-8 col-md-10 mx-auto">
-            <?php the_content(); ?>
+      <!-- Post Content -->
+      <article>
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-8 col-md-10 mx-auto">
+              <?php the_content(); ?>
+            </div>
           </div>
         </div>
-      </div>
-    </article>
-    <hr>
-  <?php endwhile; ?>
+      </article>
+      <hr>
+    <?php endwhile; ?>
+    <!-- 念の為 バグ回避 endif -->
+  <?php endif; ?>
   <!-- Footer -->
   <?php get_template_part('includes/footer'); ?>
   <?php get_footer(); ?>
