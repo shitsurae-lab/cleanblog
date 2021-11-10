1dc58468fc43c79d765e8f193369b8e0bcb5b3e7
shitsurae-lab <20566363+shitsurae-lab@users.noreply.github.com>
2021-11-10 16:19:37 +0900

    サムネイル/メニュー/アイキャッチ画像挿入を有効化

--- functions.php
+++ functions.php
@@ -4,5 +4,25 @@
 ・Command + 矢印 で行の最初と最後に移動
 */
 add_action('init', function () {
+  //サムネイルを有効化
   add_theme_support('post-thumbnails');
+  //メニューについての記述
+  register_nav_menus([
+    'global_nav' => 'グローバルナビゲーション',
+  ]);
 });
+
+/* 関数: add eyecatch アイキャッチ画像がなければ*/
+function get_default_eyecatch()
+{
+  if (has_post_thumbnail()) :
+    $id = get_post_thumbnail_id();
+    //添付された画像のurl,width,height属性を『配列』として返す。パラメータは$attachment_id,$size,$iconで $size の初期値は『thumbnail』
+    $img = wp_get_attachment_image_src($id, 'large');
+  else :
+    //上記に習って『配列』で返す必要があるため array();で囲む必要がある
+    $img = array(get_template_directory_uri() . '/img/post-bg.jpg');
+  endif;
+
+  return $img;
+}
